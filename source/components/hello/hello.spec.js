3df2773b21cf62f298fc88477a80b90ab6c74209
Andrew Romanov <me@andrew-r.ru>
2017-10-04 00:01:26 +0300

    Setup testing environment

---
+++ source/components/hello/hello.spec.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Hello from './';
+
+test('should render', () => {
+  const component = renderer.create((
+    <Hello />
+  ));
+  const tree = component.toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
