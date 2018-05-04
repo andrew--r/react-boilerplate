985efe0a38f4742c5225b3dd2a2ede3f63f7e782
Andrew Romanov <me@andrew-r.ru>
2018-05-04 19:10:45 +0300

    Update devDependencies

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,20 @@
-const postcssImport = require('postcss-import');
 const createResolver = require('postcss-import-webpack-resolver');
-const nesting = require('postcss-nesting');
-const customProperties = require('postcss-custom-properties');
-const calc = require('postcss-calc');
-const flexbugsFixes = require('postcss-flexbugs-fixes');
-const autoprefixer = require('autoprefixer');
-const csso = require('postcss-csso');
 const webpackConfig = require('./webpack.config');
 
 module.exports = {
-  plugins: [
-    postcssImport({
+  plugins: {
+    'postcss-import': {
       resolve: createResolver({
         alias: webpackConfig.resolve.alias,
       }),
-    }),
-    nesting(),
-    customProperties(),
-    calc(),
-    flexbugsFixes(),
-    autoprefixer(),
-    csso(),
-  ],
+    },
+    'postcss-nesting': {},
+    'postcss-custom-properties': {
+      preserve: false,
+    },
+    'postcss-calc': {},
+    'postcss-flexbugs-fixes': {},
+    autoprefixer: {},
+    'postcss-csso': {},
+  },
 };
